d344d83bf397bcf66709593946e243667fbcbce6
Matt Redinger <mattred1@gmail.com>
2020-06-04 18:14:20 -0400

    Add jQuery function to look like intro section is typed out

--- scripts.js
+++ scripts.js
@@ -5,3 +5,26 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
+
+
+
+
+// jQuery function to look like intro section is being typed out
+//Couldn't figure out how to have them print on separate lines 
+//Used fiddle https://jsfiddle.net/interdream/tQnG8/4/
+(function ($) {
+    $.fn.writeText = function (content) {
+        var contentArray = content.split(""),
+            current = 0,
+            elem = this;
+        setInterval(function () {
+            if (current < contentArray.length) {
+                elem.text(elem.text() + contentArray[current++]);
+            }
+        }, 200);
+    };
+
+})(jQuery);
+$("#catdad").writeText("cat dad");
+$("#nerd").writeText("D&D nerd");
+$("#fewd").writeText("front end web developer");
