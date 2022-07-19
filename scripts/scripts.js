aef36afad360de0055e950d5ee92551ffe79677e
Matt Redinger <email.mredinger@gmail.com>
2022-07-19 15:14:33 -0400

    Add new dark/light mode toggle to scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,35 @@
+// Toggle light & dark mode
+const toggleColorMode = (e) => {
+    // Switch to Light Mode
+    if (e.currentTarget.classList.contains("light--hidden")) {
+        // Sets the custom HTML attribute
+        document.documentElement.setAttribute("color-mode", "light");
+
+        //Sets the user's preference in local storage
+        localStorage.setItem("color-mode", "light")
+        return;
+    }
+
+    /* Switch to Dark Mode
+    Sets the custom HTML attribute */
+    document.documentElement.setAttribute("color-mode", "dark");
+
+    // Sets the user's preference in local storage
+    localStorage.setItem("color-mode", "dark");
+};
+
+// Get the buttons in the DOM
+const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
+
+// Set up event listeners
+toggleColorButtons.forEach(btn => {
+    btn.addEventListener("click", toggleColorMode);
+});
+
+
+// ================================================================================
+
+
 // adds background color on scroll
 $(function () {
     $(document).scroll(function () {
@@ -23,4 +55,4 @@ var nextWord = (function () {
 //Toggle dark/light mode
 function toggleDarkLight() {
     $("body").toggleClass("light-mode")
-}
+};
