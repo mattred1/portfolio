7f504d58e66974271d6913301cff28199ed0a01a
Matt Redinger <email.mredinger@gmail.com>
2022-07-21 15:12:16 -0400

    Change naming convention of color- to c-

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,23 +3,23 @@ const toggleColorMode = (e) => {
     // Switch to Light Mode
     if (e.currentTarget.classList.contains("light--hidden")) {
         // Sets the custom HTML attribute
-        document.documentElement.setAttribute("color-mode", "light");
+        document.documentElement.setAttribute("c-mode", "light");
 
         //Sets the user's preference in local storage
-        localStorage.setItem("color-mode", "light")
+        localStorage.setItem("c-mode", "light")
         return;
     }
 
     /* Switch to Dark Mode
     Sets the custom HTML attribute */
-    document.documentElement.setAttribute("color-mode", "dark");
+    document.documentElement.setAttribute("c-mode", "dark");
 
     // Sets the user's preference in local storage
-    localStorage.setItem("color-mode", "dark");
+    localStorage.setItem("c-mode", "dark");
 };
 
 // Get the buttons in the DOM
-const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
+const toggleColorButtons = document.querySelectorAll(".c-mode__btn");
 
 // Set up event listeners
 toggleColorButtons.forEach(btn => {
